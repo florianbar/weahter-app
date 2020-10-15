7bb1e095e12f64ace75b110ff4ba5fac28e104ce
florianbar <florianbar01@gmail.com>
2020-10-15 07:36:34 +0200

    Removed loading state

--- src/context/weather-context.js
+++ src/context/weather-context.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 export const WeatherContext = React.createContext({
     forecast: [],
     error: false,
-    loading: false,
     fetchForecastData: () => {},
     getWeekForecast: () => {},
     getDayForecast: () => {}
@@ -12,26 +11,21 @@ export const WeatherContext = React.createContext({
 
 export default ({ children }) => {
     const [forecast, setForecast] = useState(null);
-    const [loading, setLoading] = useState(null);
     const [error, setError] = useState(null);
 
     const fetchForecastData = useCallback(city => {
         setForecast(null);
-        setLoading(true);
         setError(false);
 
         const queryParams = `?q=${city}&appid=${process.env.REACT_APP_OPENWEATHER_APP_KEY}&units=metric`;
         axios.get("https://api.openweathermap.org/data/2.5/forecast" + queryParams)
             .then(response => {
-                //console.log(response.data);
-                setLoading(false);
                 setForecast(response.data.list);
             })
             .catch(error => {
-                setLoading(false);
                 setError(true);
             });
-    }, [setForecast, setLoading, setError]);
+    }, [setForecast, setError]);
 
     const getWeekForecast = forecast => {
         let date = new Date(forecast[0].dt_txt);
@@ -58,7 +52,6 @@ export default ({ children }) => {
     return (
         <WeatherContext.Provider value={{
             forecast: forecast,
-            loading: loading,
             error: error,
             fetchForecastData: fetchForecastData,
             getWeekForecast: getWeekForecast,
