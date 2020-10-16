1d005391df19997a8bacc4e504e0385d1814ebaa
florianbar <florianbar01@gmail.com>
2020-10-16 08:14:09 +0200

    Removed unused state

--- src/context/weather-context.js
+++ src/context/weather-context.js
@@ -2,7 +2,6 @@ import React, { useState, useCallback } from 'react';
 import axios from 'axios';
 
 export const WeatherContext = React.createContext({
-    city: null,
     forecast: [],
     error: false,
     clearErrorMessage: () => {},
@@ -12,12 +11,10 @@ export const WeatherContext = React.createContext({
 });
 
 export default ({ children }) => {
-    const [city, setCity] = useState(null);
     const [forecast, setForecast] = useState(null);
     const [error, setError] = useState(null);
 
     const fetchForecastData = useCallback(cityName => {
-        setCity(null);
         setForecast(null);
         setError(false);
 
@@ -26,13 +23,12 @@ export default ({ children }) => {
         axios.get("https://api.openweathermap.org/data/2.5/forecast" + queryParams)
             .then(response => {
                 localStorage.setItem("city", cityName);
-                setCity(cityName);
                 setForecast(response.data.list);
             })
             .catch(error => {
                 setError(error.response.data.message);
             });
-    }, [setCity, setForecast, setError]);
+    }, [setForecast, setError]);
 
     const getWeekForecast = forecast => {
         let date = new Date(forecast[0].dt_txt);
@@ -62,7 +58,6 @@ export default ({ children }) => {
 
     return (
         <WeatherContext.Provider value={{
-            city: city,
             forecast: forecast,
             error: error,
             clearErrorMessage: clearErrorMessage,
